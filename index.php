df1166e7b23903547ff3295d68cef0578b368bc9
marco candrian <mac@calmar.ws>
2016-02-20 20:53:37 +0100

    wieder hergestellt und in login orndner

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content=
+  "width=device-width, initial-scale=1.0">
+  <title>Flieger-Reservationen</title>
+  <meta name="title" content="Flieger-Reservationen">
+  <meta name="keywords" content="Reservierungs-System">
+  <meta name="description" content="Reservierungs-System">
+  <meta name="generator" content="Calmar + Vim + Tidy">
+  <meta name="owner" content="calmar.ws">
+  <meta name="author" content="candrian.org">
+  <meta name="robots" content="all">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="/reservationen/reservationen.css">
+</head>
+
+<!--[if IE]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<body>
+<?php 
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors',1);
+ini_set('html_errors', 1);
+
+require('include.php');
+
+$datum = get_date();
+$tag = $datum[0];
+$monat = $datum[1];
+$jahr = $datum[2];
+
+
+
+echo draw_calendar($tag, $monat, $jahr);
+
+?>
