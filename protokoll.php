c203f3ed3d7aee846b710e65108119ea33d65b1a
marco candrian <mac@calmar.ws>
2016-03-19 21:17:11 +0100

    protokoll.php

--- protokoll.php
+++ protokoll.php
@@ -12,6 +12,26 @@ sec_session_start();
 if (login_check($mysqli) == FALSE) { header("Location: /reservationen/login/index.php"); exit; }
 if (check_admin($mysqli) == FALSE) { header("Location: /reservationen/index.php"); exit; }
 
+if (isset($_GET['loeschen']) && $_GET['loeschen'] != 9876543210)
+{
+  if ($_GET['loeschen'] == "loggings")
+  {
+    $query = "DELETE FROM `status_meldungen`  WHERE `status_meldungen`.`aktion` = ? OR `status_meldungen`.`aktion` = ?;";
+    mysqli_prepare_execute($mysqli, $query, 'ss', array("[Eingeloggt]", "[Ausgeloggt]"));
+  }
+  else
+  {
+    $time_stamp = time();
+    $time_stamp = $time_stamp - intval($_GET['loeschen']) * 60 * 60 * 24; // tage
+
+    date_default_timezone_set("Europe/Zurich");
+    $now_string = date("Y-m-d H:i:s", $time_stamp);
+    date_default_timezone_set('UTC');
+
+    $query = "DELETE FROM `status_meldungen`  WHERE `status_meldungen`.`timestamp` < ?";
+    mysqli_prepare_execute($mysqli, $query, 's', array($now_string));
+  }
+}
 
 print_html_to_body('Log Daten', '');
 include_once('includes/usermenu.php');
@@ -23,6 +43,20 @@ include_once('includes/send_sms.php');
       <div class="center">
         <h1>Protokoll</h1>
 
+        <form action='protokoll.php' method='get'>
+          Löschen: <select style="width: 15em;" onchange='this.form.submit()' name='loeschen' size="1" id='loeschen'>
+            <option value="9876543210">                </option>
+            <option value="loggings">In-Out-Loggin's löschen</option>
+            <option value="365">älter als ein Jahr</option>
+            <option value="182">älter als ein halbes Jahr</option>
+            <option value="90">älter als 3 Monate</option>
+            <option value="30">älter als 1 Monat</option>
+            <option value="7">älter als 1 Woche</option>
+            <option value="1">älter als 1 Tag</option>
+            <option value="0">alle</option>
+          </select>
+        </form>
+
           <table class='vertical_table'>
           <tr>
           <th><b>Am</b></th>
@@ -32,7 +66,7 @@ include_once('includes/send_sms.php');
 
 <?php
 
-$query = "SELECT * FROM `status_meldungen` ORDER BY `timestamp` DESC LIMIT 100;";
+$query = "SELECT * FROM `status_meldungen` ORDER BY `timestamp` DESC;";
 
 $res = $mysqli->query($query);
 
